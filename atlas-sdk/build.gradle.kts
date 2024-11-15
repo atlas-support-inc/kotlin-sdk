820fd149cff1f96ada4f56da2dc6619ecff35ff0
Andrii Doroshko <Andr.dorosh4@gmail.com>
2024-11-14 19:02:55 -0500

    Remove Constraint Layout

--- atlas-sdk/build.gradle.kts
+++ atlas-sdk/build.gradle.kts
@@ -51,8 +51,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
 
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
